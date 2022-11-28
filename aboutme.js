74c69f33afabc33fb36dffb849da74688fc9490f
Barry Percy <baz.percy@gmail.com>
2022-11-28 12:17:03 +0000

    changed to aboutMe file

---
+++ aboutme.js
@@ -0,0 +1,10 @@
+let firstName = 'Barry';
+let lastName = 'Percy';
+let middleName = 'Dale';
+let fullName = '';
+fullName = firstName + ' ' + middleName + ' ' + lastName;
+console.log('hello, my name is', fullName);
+let myAge = 29;
+let currentYear = 2022;
+let myBirthYear = currentYear - myAge;
+console.log('My age is', myAge,'. My birth year is', myBirthYear);
